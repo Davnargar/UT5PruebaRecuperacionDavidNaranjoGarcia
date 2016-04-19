c360928e7f1b82fe997727f6b5b5a2605321a7db
1-DAM-T (2) <1-DAM-T (2)>
2016-04-19 20:15:08 +0100

    paso 3 y 4

--- src/examenrecuperacionets/ut5/Ordenador.java
+++ src/examenrecuperacionets/ut5/Ordenador.java
@@ -13,4 +13,46 @@ public class Ordenador {
     private String procesador;
     private String memoria;
     private String resolucionPantalla;
+
+    /**
+     * @return the procesador
+     */
+    public String getProcesador() {
+        return procesador;
+    }
+
+    /**
+     * @param procesador the procesador to set
+     */
+    public void setProcesador(String procesador) {
+        this.procesador = procesador;
+    }
+
+    /**
+     * @return the memoria
+     */
+    public String getMemoria() {
+        return memoria;
+    }
+
+    /**
+     * @param memoria the memoria to set
+     */
+    public void setMemoria(String memoria) {
+        this.memoria = memoria;
+    }
+
+    /**
+     * @return the resolucionPantalla
+     */
+    public String getResolucionPantalla() {
+        return resolucionPantalla;
+    }
+
+    /**
+     * @param resolucionPantalla the resolucionPantalla to set
+     */
+    public void setResolucionPantalla(String resolucionPantalla) {
+        this.resolucionPantalla = resolucionPantalla;
+    }
 }
